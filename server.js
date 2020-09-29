4e56fc754b525cfb926b5c9224b59ab7ce0a275e
Panupat-Chanate <panupat.c@psru.ac.th>
2020-09-29 23:48:45 +0700

    mongodb

--- server.js
+++ server.js
@@ -6,14 +6,41 @@ const PORT = process.env.PORT || 5000;
 
 const mongoUri = 'mongodb+srv://panu_backend:15515511@clusterdb.1wuju.mongodb.net/TutorialDB?retryWrites=true&w=majority';
 const db = mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true});
+const Data = mongoose.model('tb_alumni', { _id: String });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    Data.find({
+    })
+    .select('_id firstName lastName')
+    .exec(function(err, datanames) {
+        if (err) {
+            return next(err);
+        }else{
+            res.json(datanames);
+        }
+    });
 });
 
 app.post('/check', (req, res) => {
-    res.json('TEST ROUTE!');
-    console.log("Hello")
+    console.log(req.body.checkId)
+    Data.findOne({_id: req.body.checkId}, (err, dataId) => {
+        if (err) return console.log(err)
+        if (dataId === null) {
+            console.log("ไม่มี");
+            var checked ={
+                checkedState: false
+            };
+            console.log(checked);
+            res.json(checked);
+        } else {
+            console.log('มีแล้ว');
+            var checked ={
+                checkedState: true
+            };
+            console.log(checked);
+            res.json(checked);
+        }
+    })
 });
 
 app.listen(PORT, () => {
